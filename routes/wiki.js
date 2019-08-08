6fb149708f3896b4ec503c2419c2eb9f361f1932
Daniel Fontana <dfont110@gmail.com>
2019-08-08 16:59:53 -0500

    last commit

--- routes/wiki.js
+++ routes/wiki.js
@@ -12,16 +12,32 @@ router.get('/add', (req, res, next) => {
   res.send(addPage());
 });
 
+router.get('/:slug', async (req, res, next) => {
+  try {
+    // const page = await Page.findAll();
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug,
+      },
+    });
+    console.log('page', page);
+    res.json(page);
+  } catch (error) {
+    next(error);
+  }
+  // res.send(`hit dynamic route at ${req.params.slug}`);
+});
+
 router.post('/', async (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
 
   const page = new Page({
     title: title,
-    content: content
+    content: content,
   });
   // res.json(req.body);
-
+  console.log(page);
   try {
     await page.save();
     res.redirect('/');
