cd944fee9b9a00614eeb78e49942b710845f78e1
Christine Vargas <christineandreavargas@gmail.com>
2019-08-08 11:49:57 -0500

    sequelize essentials

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const express = require('express');
 const morgan = require('morgan');
-const app = express();
 const bodyParser = require('body-parser');
+const { db } = require('./models');
+const models = require('./models');
+
+// db.authenticate().then(() => {
+//   console.log('connected to the db');
+// });
+const app = express();
 
 app.use(express.static(__dirname + '/public'));
-
 app.use(express.urlencoded({ extended: false }));
 
+const layout = require('./views/layout.js');
+
 app.get('/', (req, res) => {
-  res.send(console.log('hello world'));
+  res.send(layout(' '));
 });
 
-const PORT = 1234;
-app.listen(PORT, () => {
-  console.log(`listening at ${PORT}`);
-});
+const connect = async () => {
+  await models.db.sync({ force: true });
+
+  const PORT = 1234;
+  app.listen(PORT, () => {
+    console.log(`listening at ${PORT}`);
+  });
+};
+connect();
